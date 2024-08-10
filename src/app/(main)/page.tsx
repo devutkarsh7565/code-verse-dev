d2a69c263abc2ef11df2cb17fed985dbafb1ca70
devutkarsh7565 <utkarshtiwari200389@gmail.com>
2024-08-10 13:24:29 +0530

    add navabar section of the landing page of app

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import { DarkMode } from "../components/DarkMode/DarkMode";
+import AppIconWrapper from "../components/SideBarWrapper/AppIconWrapper";
 
 const Home = () => {
   return (
-    <div>
-      <DarkMode />
+    <div className="flex flex-col w-full h-full p-8">
+      <div className="flex items-center justify-between w-full">
+        <AppIconWrapper />
+        <div className="flex gap-3 items-center">
+          <button className="py-2 px-5 text-xs font-medium text-purple-50 bg-purple-600 rounded-md duration-300 hover:bg-purple-500 hover:text-white">
+            Sign In
+          </button>
+          <button className="py-2 px-5 text-xs font-medium border border-purple-500 hover:border-transparent focus:border-purple-600 text-purple-500 bg-purple-50 rounded-md duration-300 hover:bg-purple-600 hover:text-purple-50">
+            Sign Up
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
